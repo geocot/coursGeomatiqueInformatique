8afadad1eb8697bcf6ca3c7ff0a696e147b34305
Martin C <martin.couture@uqtr.ca>
2021-03-03 16:58:40 -0500

    Ajout

---
+++ Semaine 6/demo.js
@@ -0,0 +1,14 @@
+let geom = "polygone";
+
+switch(geom) {
+    case "point":
+        alert("La géommétrie est de type point");
+    case "polyligne":
+        alert("La géommétrie est de type polyligne");
+    case "polygone":
+        alert("La géommétrie est de type polygone");
+    default:
+        alert("La géométrie n'est pas de type point, polyligne ou polygone")
+    }
+
+    
